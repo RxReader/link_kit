f07c9e6169e09e6056777db8d67580d7eb502c2c
v7lin <v7lin@qq.com>
2022-07-26 18:11:24 +0800

    android link kit

--- README.md
+++ README.md
@@ -2,6 +2,35 @@
 
 Flutter plugin for App/Deep Link.
 
+## Android
+
+* 配置
+
+```xml
+<activity
+    android:name=".MainActivity">
+    <intent-filter android:autoVerify="false">
+        <action android:name="android.intent.action.VIEW"/>
+        <category android:name="android.intent.category.DEFAULT"/>
+        <category android:name="android.intent.category.BROWSABLE"/>
+        <!-- 固定标志 -->
+        <category android:name="${applicationId}.link_kit.category.FLK"/>
+        <!-- scheme 为必选项，host 为可选项，scheme/host 可自定义 -->
+        <data android:scheme="flk" android:host="link.kit"/>
+    </intent-filter>
+</activity>
+```
+
+* 测试
+
+```shell
+adb shell am start -a android.intent.action.VIEW \
+    -c android.intent.category.BROWSABLE \
+    -d "flk://link.kit/power"
+```
+
+## iOS
+
 ## Getting Started
 
 This project is a starting point for a Flutter
--- android/src/main/java/io/github/v7lin/link_kit/LinkKitPlugin.java
+++ android/src/main/java/io/github/v7lin/link_kit/LinkKitPlugin.java
@@ -1,38 +1,118 @@
 package io.github.v7lin.link_kit;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+
 import androidx.annotation.NonNull;
 
+import java.util.List;
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry;
 
-/** LinkKitPlugin */
-public class LinkKitPlugin implements FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private MethodChannel channel;
+/**
+ * LinkKitPlugin
+ */
+public class LinkKitPlugin implements FlutterPlugin, ActivityAware, PluginRegistry.NewIntentListener, MethodCallHandler {
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private MethodChannel channel;
+    private Context applicationContext;
+    private ActivityPluginBinding activityPluginBinding;
 
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "link_kit");
-    channel.setMethodCallHandler(this);
-  }
+    // --- FlutterPlugin
 
-  @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+        channel = new MethodChannel(binding.getBinaryMessenger(), "v7lin.github.io/link_kit");
+        channel.setMethodCallHandler(this);
+        applicationContext = binding.getApplicationContext();
     }
-  }
 
-  @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    channel.setMethodCallHandler(null);
-  }
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+        channel = null;
+        applicationContext = null;
+    }
+
+    // --- ActivityAware
+
+    @Override
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+        activityPluginBinding = binding;
+        activityPluginBinding.addOnNewIntentListener(this);
+    }
+
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
+        onDetachedFromActivity();
+    }
+
+    @Override
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+        onAttachedToActivity(binding);
+    }
+
+    @Override
+    public void onDetachedFromActivity() {
+        activityPluginBinding.removeOnNewIntentListener(this);
+        activityPluginBinding = null;
+    }
+
+    // --- NewIntentListener
+
+    @Override
+    public boolean onNewIntent(@NonNull Intent intent) {
+        if (isFLKIntent(intent)) {
+            final String link = intent.getDataString();
+            if (channel != null) {
+                channel.invokeMethod("onClickLink", link);
+            }
+        }
+        return false;
+    }
+
+    // --- MethodCallHandler
+
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        if (call.method.equals("getInitialLink")) {
+            String initialLink = null;
+            final Activity activity = activityPluginBinding.getActivity();
+            if (activity != null) {
+                final Intent intent = activity.getIntent();
+                if (isFLKIntent(intent)) {
+                    initialLink = intent.getDataString();
+                }
+            }
+            result.success(initialLink);
+        } else {
+            result.notImplemented();
+        }
+    }
+
+    private boolean isFLKIntent(Intent intent) {
+        final Intent copy = new Intent(intent);
+        copy.setComponent(null);// 必须设置为空，不然无法获取 ResolveInfo 的 IntentFilter
+        final List<ResolveInfo> infos = applicationContext.getPackageManager().queryIntentActivities(copy, PackageManager.GET_RESOLVED_FILTER);
+        if (infos.size() == 1) {
+            final ResolveInfo info = infos.get(0);
+            final IntentFilter filter = info.filter;
+            return filter != null && filter.hasCategory(applicationContext.getPackageName() + ".link_kit.category.FLK");
+        }
+        return false;
+    }
 }
